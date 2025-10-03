4d097d548e9b407a325d791f41c99dd8c04256d3
ejim11 <favourejim56@gmail.com>
2025-10-03 22:21:11 +0100

    ✨ added the user module and providers : CreateUserProvider and FindUserByEmailProvider. Also created a circular dependency with the auth module.

---
+++ server/src/users/users.module.ts
@@ -0,0 +1,16 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './providers/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { AuthModule } from 'src/auth/auth.module';
+import { CreateUserProvider } from './providers/create-user.provider';
+import { FindUserByEmailProvider } from './providers/find-user-by-email.provider';
+
+@Module({
+  imports: [forwardRef(() => AuthModule), TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UsersService, CreateUserProvider, FindUserByEmailProvider],
+  exports: [UsersService],
+})
+export class UsersModule {}
