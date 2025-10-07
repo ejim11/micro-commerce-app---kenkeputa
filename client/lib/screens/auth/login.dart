e358205d6de0490cb8ed669d787d2549aeb3c1ae
ejim11 <favourejim56@gmail.com>
2025-10-07 11:02:08 +0100

    ✨ added the login screen

---
+++ client/lib/screens/auth/login.dart
@@ -0,0 +1,83 @@
+import "package:client/widgets/login_form.dart";
+import "package:flutter/gestures.dart";
+import "package:flutter/material.dart";
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
+        title: const Text('Sign In'),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight:
+                  MediaQuery.of(context).size.height -
+                  AppBar().preferredSize.height -
+                  MediaQuery.of(context).padding.top,
+            ),
+            child: IntrinsicHeight(
+              child: Container(
+                padding: const EdgeInsets.only(
+                  top: 40,
+                  bottom: 30,
+                  left: 20,
+                  right: 20,
+                ),
+                child: Column(
+                  children: [
+                    const LoginForm(),
+                    const SizedBox(height: 15),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 15,
+                      ),
+                      child: RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          text: "Don't have an account? ",
+                          style: Theme.of(context).textTheme.bodyMedium!
+                              .copyWith(
+                                color: const Color.fromRGBO(134, 134, 134, 0.7),
+                                fontSize: 14,
+                              ),
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: 'Sign Up',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.onPrimary,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  // Navigator.pushReplacement(
+                                  //   context,
+                                  //   MaterialPageRoute(
+                                  //     builder: (ctx) => const Register(),
+                                  //   ),
+                                  // );
+                                },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
