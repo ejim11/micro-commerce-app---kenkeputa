43b0090b3ebae62a8bc399f97f5ec5380a2f1621
ejim11 <favourejim56@gmail.com>
2025-10-07 11:21:21 +0100

    🗃️ added the order entity

---
+++ server/src/orders/order.entity.ts
@@ -0,0 +1,53 @@
+import { User } from 'src/users/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { OrderItem } from './order-item.entity';
+import { OrderStatus } from './enums/order-status.enum';
+
+@Entity('orders')
+export class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  total: number;
+
+  @Column({ type: 'enum', enum: OrderStatus, default: OrderStatus.PENDING })
+  status: OrderStatus;
+
+  @Column({
+    type: 'varchar',
+  })
+  shippingAddress: string;
+
+  @Column({
+    type: 'varchar',
+  })
+  billingAddress: string;
+
+  @Column({
+    type: 'varchar',
+    length: 100,
+    default: 'Mocked Payment Method',
+  })
+  paymentMethod: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.orders, { onDelete: 'CASCADE' })
+  user: User;
+
+  @OneToMany(() => OrderItem, (orderItem) => orderItem.order)
+  items: OrderItem[];
+}
