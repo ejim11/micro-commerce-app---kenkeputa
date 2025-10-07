cd17f42d2317611be47b93063d803b6bbbfe6a84
ejim11 <favourejim56@gmail.com>
2025-10-07 11:38:47 +0100

    ✅ added the find all products provider

---
+++ server/src/products/providers/find-all-products.provider.ts
@@ -0,0 +1,96 @@
+import { Injectable } from '@nestjs/common';
+import { GetProductsDto } from '../dtos/get-products.dto';
+import { Paginated } from 'src/common/pagination/interfaces/paginated.interface';
+import { Product } from '../product.entity';
+import { Between, FindOptionsOrder, In, Like, Repository } from 'typeorm';
+import { PaginationQueryDto } from 'src/common/pagination/dtos/pagination-query.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
+
+@Injectable()
+export class FindAllProductsProvider {
+  constructor(
+    /**
+     * injecting the products repository
+     */
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>,
+
+    /**
+     * injecting the pagination provider
+     */
+    private readonly paginationProvider: PaginationProvider,
+  ) {}
+
+  findAllProducts(productQuery: GetProductsDto): Promise<Paginated<Product>> {
+    const cleanedQuery = this.cleanQuery(productQuery);
+
+    const { limit, page, sort } = cleanedQuery;
+
+    const safeLimit = Number(limit) > 0 ? Number(limit) : 10;
+    const safePage = Number(page) > 0 ? Number(page) : 1;
+
+    const where: any = {
+      isDeleted: false,
+    };
+
+    if (cleanedQuery.category) {
+      where.category = In(cleanedQuery.category.split(','));
+    }
+    if (cleanedQuery.price) {
+      const [min, max] = cleanedQuery.price
+        .split('-')
+        .map((p) => parseFloat(p.trim()));
+
+      if (!isNaN(min) && !isNaN(max)) {
+        where.price = Between(min, max);
+      }
+    }
+    if (cleanedQuery.name) {
+      const searchTerm = cleanedQuery.name.split('-').join(' '); // Replace dashes with spaces
+      where.name = Like(`%${searchTerm}%`);
+    }
+
+    // Build order conditions (default to newest)
+    const order: FindOptionsOrder<Product> = { createdAt: 'DESC' };
+    if (sort) {
+      switch (sort) {
+        case 'newest':
+          order.createdAt = 'DESC';
+          break;
+        case 'oldest':
+          order.createdAt = 'ASC';
+          break;
+
+        default:
+          // Fallback if validation misses
+          order.createdAt = 'DESC';
+      }
+    }
+
+    // Use PaginationProvider with options
+    const paginationQuery: PaginationQueryDto = {
+      limit: safeLimit,
+      page: safePage,
+    };
+
+    return this.paginationProvider.paginationQuery<Product>(
+      paginationQuery,
+      this.productsRepository,
+      {
+        where,
+        order,
+      },
+    );
+  }
+
+  private cleanQuery(query: GetProductsDto): GetProductsDto {
+    return Object.fromEntries(
+      Object.entries(query).filter(
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        ([_, value]) =>
+          value !== 'undefined' && value !== undefined && value !== '',
+      ),
+    ) as GetProductsDto;
+  }
+}
