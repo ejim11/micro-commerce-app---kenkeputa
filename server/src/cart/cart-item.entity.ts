f8984d3746e4cad2063649f750ec5d2280135c4a
ejim11 <favourejim56@gmail.com>
2025-10-07 11:12:31 +0100

    🗃️ added the cart item entity

---
+++ server/src/cart/cart-item.entity.ts
@@ -0,0 +1,39 @@
+// src/cart/entities/cart-item.entity.ts
+// User-based cart item entity: Persists cart per user in DB
+import { Product } from 'src/products/product.entity';
+import { User } from 'src/users/user.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  DeleteDateColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('cart_items')
+export class CartItem {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ default: 1 })
+  quantity: number;
+
+  @ManyToOne(() => User, (user) => user.cartItems, { onDelete: 'CASCADE' })
+  user: User;
+
+  @ManyToOne(() => Product, (product) => product.cartItems, {
+    onDelete: 'CASCADE',
+  })
+  product: Product;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt?: Date; // Soft delete for cart items
+}
