334885d00477b41a2c30a450fc696e62be97f5bd
ejim11 <favourejim56@gmail.com>
2025-10-08 10:54:01 +0100

    🗃️ updated the order item entity

--- server/src/orders/order-item.entity.ts
+++ server/src/orders/order-item.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Order } from './order.entity';
-import { Product } from 'src/products/product.entity';
+import { Product } from '../products/product.entity';
 
 @Entity('order_items')
 export class OrderItem {
