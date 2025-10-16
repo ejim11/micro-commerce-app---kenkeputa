de37b8d2c0d2e6e96267000495d823665f9318a5
ejim11 <favourejim56@gmail.com>
2025-10-16 19:11:30 +0100

    📝 added the swagger doc for the create order dto

--- server/src/orders/dtos/create-order.dto.ts
+++ server/src/orders/dtos/create-order.dto.ts
@@ -1,14 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateOrderDto {
+  @ApiProperty({
+    description: 'user shipping address',
+    example: 'enugu, nigeria',
+  })
   @IsNotEmpty()
   @IsString()
   shippingAddress: string;
 
+  @ApiProperty({
+    description: 'user billing address',
+    example: 'enugu, nigeria',
+  })
   @IsNotEmpty()
   @IsString()
   billingAddress: string;
 
+  @ApiProperty({
+    description: 'payment method',
+    example: 'credit_card',
+  })
   // Optional: Payment method (mocked)
   @IsOptional()
   @IsString()
