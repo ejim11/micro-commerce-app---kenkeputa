d01b2024a46fb215889534f87c36ee9d060822d8
ejim11 <favourejim56@gmail.com>
2025-10-16 19:10:46 +0100

    📝 swagger doc for the dto

--- server/src/common/pagination/dtos/pagination-query.dto.ts
+++ server/src/common/pagination/dtos/pagination-query.dto.ts
@@ -1,4 +1,5 @@
 // import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsPositive } from 'class-validator';
 
 /**
@@ -8,6 +9,10 @@ export class PaginationQueryDto {
   /**
    * limit for the number of items in the get all requests
    */
+  @ApiProperty({
+    description: 'pagination limit',
+    example: 10,
+  })
   @IsOptional()
   limit?: number = 0;
 
